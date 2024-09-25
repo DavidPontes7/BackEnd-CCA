adf9628a26ddafd14c69251c008fbf2ac0b23bb2
DavidPontes7 <pontesdejesusdavid@gmail.com>
2024-09-25 15:38:19 -0300

    kkkkk

--- src/modules/administrador/application/administrador.application.exception.ts
+++ src/modules/administrador/application/administrador.application.exception.ts
@@ -1,4 +1,4 @@
-import { ApplicationException } from ".../../shared/application/application.exception";
+import { ApplicationException } from "../../../shared/application/application.exception";
 
 class AdmApplicationException extends ApplicationException {
     constructor(message:string = '⚠️ Exceção de Aplicação Genérica da Entidade Administrador') {
